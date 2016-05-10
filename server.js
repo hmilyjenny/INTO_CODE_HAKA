a774082a340ef46af4fff55040fb6a71840677d9
Jason <hmilyjenny@gmail.com>
2016-05-10 22:51:03 +0800

    add index.html

---
+++ server.js
@@ -0,0 +1,18 @@
+import Express from 'express';
+import path from 'path';
+
+var express = require("express");
+var app = new Express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
+
+var server=app.listen(serverConfig.port, (error) => {
+  if (!error) {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  }
+});
---
+++ webpack-prod.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+          '.client/src/index.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  module:{
+    loaders:[
+      {
+        test: /\.jsx*$/,
+        exclude: [/node_modules/, /.+\.config.js/],
+        loader: 'babel',
+      },
+      {
+        test: /\.css$/,
+        loaders:[
+          'style-loader?sourceMap',
+          'css'
+        ]
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)([\?]?.*)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  plugins:[
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+}
